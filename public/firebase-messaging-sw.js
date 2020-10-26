3061ced9f88cbdd5981aee0d507c6c9216554b09
Astitva Gupta <gupta.astitva@gmail.com>
2020-10-27 02:22:14 +0530

    Disabled warnings

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable no-undef */
 importScripts('https://www.gstatic.com/firebasejs/3.5.0/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/3.5.0/firebase-messaging.js');
